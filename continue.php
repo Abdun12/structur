b3137b948fcaeb04a6569569b9442ada7aa9cec4
unknown <mabdun120604@gmail.com>
2023-05-31 11:34:37 +0700

    project Structur

---
+++ continue.php
@@ -0,0 +1,10 @@
+<?php
+    for($i = 1 ; $i <= 10 ; $i++)
+    {
+        if($i == 5)
+        {
+            continue;//melewati iterasi ke-5
+        }
+        echo $i."";
+    }
+?>
---
+++ foreach.php
@@ -0,0 +1,8 @@
+<?php
+    $nama = array(“Adi”, “Budi”, “Carli”, “Dedy”);
+    foreach($nama as $val)
+    {
+        echo "$val";
+        echo "<br/>";
+    }
+?>
